edff06422067031c80138897598875a5c8a098f7
Supriya <supriya@learnapp.co>
2019-01-04 23:50:57 +0530

    first commit

--- src/router.js
+++ src/router.js
@@ -1,25 +1,52 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
+
+function getRouteLayout(name) {
+  return () => import(`@/layouts/${name}`);
+}
+
+function getRouteComponent(name) {
+  return () => import(`@/modules/${name}/${name}`);
+}
+
+const DefaultLayout = getRouteLayout('DefaultLayout');
+// const DashboardLayout = getRouteLayout('DashboardLayout');
+
+const routes = [
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '/',
+        redirect: 'home',
+      },
+      {
+        path: 'home',
+        component: getRouteComponent('Home'),
+      },
+    ],
+  },
+  {
+    path: '/dashboard',
+    component: getRouteComponent('Dashboard'),
+  },
+  {
+    path: '/auth',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '/login',
+        component: getRouteComponent('Login'),
+      },
+    ],
+  },
+];
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+  routes,
 });
